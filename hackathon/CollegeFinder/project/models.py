97b840d79d8a11638d61fc1ba0bc67dfad27a8a0
aboudehhub <53794997+aboudehhub@users.noreply.github.com>
2021-01-23 01:29:01 +0200

    fixes

--- hackathon/CollegeFinder/project/models.py
+++ hackathon/CollegeFinder/project/models.py
@@ -2,3 +2,10 @@ from django.db import models
 
 # Create your models here.
 
+class College(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    location = models.CharField(max_length=200, null=True)
+    fees = models.IntegerField(null=True)
+    
+    def __str__(self):
+        return self.name
--- hackathon/CollegeFinder/project/urls.py
+++ hackathon/CollegeFinder/project/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.Home),
     path('Chats/', views.Chats),
     path('HowItWorks/', views.HowItWorks),
